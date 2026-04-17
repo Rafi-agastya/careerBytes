cc251997e22498369fc99651c63ca1ef173215c2
Shabrina Hikmah <shabrinahshaf@gmail.com>
2026-04-18 01:23:36 +0700

    chore: setup eslint and fix drizzle types

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,9 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({connectionString: process.env.DATABASE_URL});
 
-pool
-  .connect()
-  .then(() => console.log('Database Connected'))
-  .catch((err: Error) => console.error('Database Connection error: ', err));
-export default pool;
+pool.connect()
+.then(() => console.log('Database Connected'))
+.catch((err: Error) => console.error('Database Connection error: ', err))
+export default pool; 
